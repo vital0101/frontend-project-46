5adb9d58c95197688bf9ab4b50b2e03881569976
Vitaliy Vaskin <vital0101@yandex.ru>
2023-05-28 13:32:14 +0300

    Refacoring gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import { getGenDiff } from '../src/index.js';
+
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  // .option('-h, --help', 'display help for command')
+  .option('-f, --format <type>', 'output format')
+  // .arguments('<filepath1, <filepath2')
+  .arguments('<filepath1>', 'path to file1')
+  .arguments('<filepath1>', 'path to file2')
+  .action((filepath1, filepath2) => {
+    console.log(getGenDiff(filepath1, filepath2));
+  })
+  .parse(process.argv);
