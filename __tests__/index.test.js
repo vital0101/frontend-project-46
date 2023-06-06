e3ca3186e74ab97711514f343e01296fac517f31
Vitaliy Vaskin <vital0101@yandex.ru>
2023-06-06 17:36:59 +0300

    Added index.test.js

---
+++ __tests__/index.test.js
@@ -0,0 +1,27 @@
+import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import fs from 'fs';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getPath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+
+const result = fs.readFileSync(
+  `${__dirname}/../__fixtures__/expected_file.txt`,
+  'utf-8',
+);
+
+test('generate difference between JSONs files', () => {
+  expect(genDiff(getPath('file1.json'), getPath('file2.json'))).toEqual(result);
+});
+
+// test('generate difference between YML/YAMLs files', () => {
+//   expect(genDiff(getPath('file1.yml'), getPath('file2.yaml'))).toEqual(result);
+// });
+
+// test('generate difference between JSON and YML/YAML files', () => {
+//   expect(genDiff(getPath('file1.json'), getPath('file2.yaml'))).toEqual(result);
+// });
