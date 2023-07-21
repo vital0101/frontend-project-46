9b4a2a5b73b58a7b581ad000a93da5708d894ff8
Vitaliy Vaskin <vital0101@yandex.ru>
2023-07-21 10:27:14 +0300

    Added tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,27 +1,42 @@
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import path, { dirname } from 'path';
 import fs from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getPath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+const getPath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getPath(filename), 'utf-8');
 
-const result = fs.readFileSync(
-  `${__dirname}/../__fixtures__/expected_file.txt`,
-  'utf-8',
-);
+// const resultPlain = readFile('expectedPlain');
+// const resultJson = readFile('expectedJson');
+const resultStylish = readFile('expectedStylish.txt');
 
 test('generate difference between JSONs files', () => {
-  expect(genDiff(getPath('file1.json'), getPath('file2.json'))).toEqual(result);
+  const actual1 = genDiff(
+    getPath('file1.json'),
+    getPath('file2.json'),
+    'stylish',
+  );
+  expect(actual1).toEqual(resultStylish);
 });
 
 test('generate difference between YML/YAMLs files', () => {
-  expect(genDiff(getPath('file1.yml'), getPath('file2.yaml'))).toEqual(result);
+  const actual2 = genDiff(
+    getPath('file1.yml'),
+    getPath('file2.yaml'),
+    'stylish',
+  );
+  expect(actual2).toEqual(resultStylish);
 });
 
-// test('generate difference between JSON and YML/YAML files', () => {
-//   expect(genDiff(getPath('file1.json'), getPath('file2.yaml'))).toEqual(result);
-// });
+test('generate difference between JSON and YML/YAML files', () => {
+  const actual3 = genDiff(
+    getPath('file1.json'),
+    getPath('file2.yaml'),
+    'stylish',
+  );
+  expect(actual3).toEqual(resultStylish);
+});
