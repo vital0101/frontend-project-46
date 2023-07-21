e2ffe0454763cf586b4c0fc2d559cc69dc749389
Vitaliy Vaskin <vital0101@yandex.ru>
2023-07-21 10:27:43 +0300

    Changed file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,12 +7,10 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  // .option('-h, --help', 'display help for command')
-  .option('-f, --format <type>', 'output format')
-  // .arguments('<filepath1, <filepath2')
+  .option('-f | --format <type>', 'output format')
   .arguments('<filepath1>', 'path to file1')
   .arguments('<filepath2>', 'path to file2')
-  .action((filepath1, filepath2) => {
-    getGenDiff(filepath1, filepath2);
+  .action((filepath1, filepath2, options) => {
+    console.log(getGenDiff(filepath1, filepath2, options.format));
   })
   .parse(process.argv);
