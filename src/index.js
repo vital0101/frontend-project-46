007633b2391c220756abaced51eea3974bbbfd7a
Vitaliy Vaskin <vital0101@yandex.ru>
2023-07-24 19:56:05 +0300

    Change names variabeles

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import parser from './parsers.js';
 import getDiff from './getDiff.js';
 import getFormatedFile from './formatters/index.js';
 
-const getPathOfFile = (filePath) => path.resolve(filePath);
-const getTypeOfFile = (filePath) => path.extname(filePath);
-const getDataFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
+const getFilePath = (filePath) => path.resolve(filePath);
+const getFileType = (filePath) => path.extname(filePath);
+const getFileData = (filePath) => fs.readFileSync(filePath, 'utf-8');
 
 const getGenDiff = (filePath1, filePath2, format = 'stylish') => {
-  const pathOfFile1 = getPathOfFile(filePath1);
-  const pathOfFile2 = getPathOfFile(filePath2);
+  const pathOfFile1 = getFilePath(filePath1);
+  const pathOfFile2 = getFilePath(filePath2);
 
-  const extensionOfFile1 = getTypeOfFile(pathOfFile1);
-  const extensionOfFile2 = getTypeOfFile(pathOfFile2);
+  const extensionOfFile1 = getFileType(pathOfFile1);
+  const extensionOfFile2 = getFileType(pathOfFile2);
 
-  const dataFile1 = getDataFile(filePath1);
-  const dataFile2 = getDataFile(filePath2);
+  const dataFile1 = getFileData(filePath1);
+  const dataFile2 = getFileData(filePath2);
 
   const dataParse1 = parser(dataFile1, extensionOfFile1);
   const dataParse2 = parser(dataFile2, extensionOfFile2);
