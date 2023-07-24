ba712f64295ab85157b9ceecd67db8ea9275c7cc
Vitaliy Vaskin <vital0101@yandex.ru>
2023-07-24 19:49:30 +0300

    Refactoring file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+import { program } from 'commander';
 import getGenDiff from '../src/index.js';
-
-const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
