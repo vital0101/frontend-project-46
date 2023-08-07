a5e81088705d3f02880193c509031fbbc82c5586
Vitaliy Vaskin <vital0101@yandex.ru>
2023-08-07 14:28:43 +0300

    Refactoring file

--- src/parsers.js
+++ src/parsers.js
@@ -1,20 +1,6 @@
 import yaml from 'js-yaml';
 
 const parser = (data, type) => {
-  // const typeFile = {
-  //   json: (fileData) => JSON.parse(fileData),
-  //   yml: (fileData) => yaml.load(fileData),
-  //   yaml: (fileData) => yaml.load(fileData),
-  // };
-  // switch (type) {
-  //   case '.json':
-  //     return JSON.parse(data);
-  //   case '.yml':
-  //   case '.yaml':
-  //     return yaml.load(data);
-  //   default:
-  //     throw new Error(`File extension ${type} is incorrect`);
-  // }
   const typeFile = {
     json: '.json',
     yml: '.yml',
