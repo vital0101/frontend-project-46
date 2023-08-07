cbd1988190b210d070e52fdc38ee4620a95984e3
Vitaliy Vaskin <vital0101@yandex.ru>
2023-08-07 14:27:48 +0300

    Refactoring file

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,16 +2,22 @@ import plain from './plain.js';
 import stylish from './stylish.js';
 
 const getFormatedFile = (tree, format = 'stylish') => {
-  switch (format) {
-    case 'json':
-      return JSON.stringify(tree);
-    case 'plain':
-      return plain(tree);
-    case 'stylish':
-      return stylish(tree);
-    default:
-      return new Error(`Type '${format}' is not supported`);
+  const formatType = {
+    stylish: 'stylish',
+    plain: 'plain',
+    json: 'json',
+  };
+
+  if (format === formatType.json) {
+    return JSON.stringify(tree);
   }
+  if (format === formatType.plain) {
+    return plain(tree);
+  }
+  if (format === formatType.stylish) {
+    return stylish(tree);
+  }
+  return new Error(`Type '${format}' is not supported`);
 };
 
 export default getFormatedFile;
--- src/parsers.js
+++ src/parsers.js
@@ -6,15 +6,28 @@ const parser = (data, type) => {
   //   yml: (fileData) => yaml.load(fileData),
   //   yaml: (fileData) => yaml.load(fileData),
   // };
-  switch (type) {
-    case '.json':
-      return JSON.parse(data);
-    case '.yml':
-    case '.yaml':
-      return yaml.load(data);
-    default:
-      throw new Error(`File extension ${type} is incorrect`);
+  // switch (type) {
+  //   case '.json':
+  //     return JSON.parse(data);
+  //   case '.yml':
+  //   case '.yaml':
+  //     return yaml.load(data);
+  //   default:
+  //     throw new Error(`File extension ${type} is incorrect`);
+  // }
+  const typeFile = {
+    json: '.json',
+    yml: '.yml',
+    yaml: '.yaml',
+  };
+
+  if (type === typeFile.json) {
+    return JSON.parse(data);
   }
+  if (type === typeFile.yml || type === typeFile.yaml) {
+    return yaml.load(data);
+  }
+  return new Error(`File extension ${type} is incorrect`);
 };
 
 export default parser;
